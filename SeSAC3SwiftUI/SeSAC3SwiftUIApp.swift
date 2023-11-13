9111f2f75d291b2386dd1f9b5469be9d2e2666f2
Lavender <susie204@naver.com>
2023-11-13 09:43:56 +0900

    Initial Commit

---
+++ SeSAC3SwiftUI/SeSAC3SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSAC3SwiftUIApp.swift
+//  SeSAC3SwiftUI
+//
+//  Created by 이승현 on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SeSAC3SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
